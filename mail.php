e75fa8167c9dc831638d6aed1e8f6ce483e473f1
Moses Ichado <ichadomoses@gmail.com>
2022-03-22 15:10:58 +0100

    added email functionality

---
+++ mail.php
@@ -0,0 +1,20 @@
+<?php
+//get data from form  
+
+$name = $_POST['name'];
+$email= $_POST['email'];
+$company= $_POST['company'];
+$inquiry= $_POST['inquiry'];
+
+$to = " clifford@giginov.com";
+$subject = "Mail From giginov website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Company =" . $company . "\r\n Inquiry =" . $inquiry;
+
+$headers = "From: noreply@giginov.com" . "\r\n" .
+"CC: moses@giginov.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>

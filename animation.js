cce81889d0d5b6bcdde4c0cc934da476d7109124
Moses Ichado <ichadomoses@gmail.com>
2022-03-22 12:54:35 +0100

    updated landing page and  footer responsiveness

--- animation.js
+++ animation.js
@@ -1,7 +1,7 @@
 for (let i = 1; i <= 9; i++)
 ScrollReveal().reveal(`#sv${i}`, {
-    duration: 1000,
+    duration: 500,
     reset: false,
     scale: 0,
-    delay: 300 + i * 200,
+    delay: 200 + i * 50,
 });
